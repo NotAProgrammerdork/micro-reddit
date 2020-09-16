dc064e4034eb11c846826933373a9609f0884f08
Dork <imnotaprogrammer@protonmail.com>
2020-09-15 22:03:28 -0500

    Add Comment/Post-User relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
+  has_many :posts
+  has_many :comments
   validates :username, :email, :password, presence: true
   validates :username, uniqueness: true, length: { in: 5..15 }
   validates :password, length: { minimum: 6 }
-  has_many :posts
 end
