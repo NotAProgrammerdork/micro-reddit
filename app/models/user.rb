cd8d44945ba9329ce3d88a0a8dd5d0b031056f3d
Dork <imnotaprogrammer@protonmail.com>
2020-09-15 19:47:01 -0500

    Create User validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :username, :email, :password, presence: true
+  validates :username, uniqueness: true, length: { in: 5..15 }
+  validates :password, length: { minimum: 6 }
 end
